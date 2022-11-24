8306a2095957428401720b32c99512858415db81
amr <amr.emish@startappz.com>
2022-11-24 13:54:11 +0200

    apply deep update

--- src/creator.py
+++ src/creator.py
@@ -132,27 +132,22 @@ class SpaceCreator:
         meta = mapper.get('columns_mapper').get('meta')
         body = mapper.get('columns_mapper').get('body')
         schema_shortname = mapper.get('dest').get('schema_shortname')
-
-        meta_data: dict = {}
-        for meta_name in meta.keys():
-            col_name = meta[meta_name]
-            if col_name not in row_data:
-                raise Exception(f'column {col_name} is not found in row: {row_data}')
-            meta_data[meta_name] = row_data[col_name]
-
-        body_data: dict = {}
-        for body_name in body.keys():
-            col_name = body[body_name]
-            if col_name not in row_data:
-                raise Exception(f'column {col_name} is not found in row: {row_data}')
-            body_data[body_name] = row_data[col_name]
-
-        meta_data['owner_shortname'] = 'dmart'
+        meta_data: dict = self.deep_update(meta, row_data)
+        body_data: dict = self.deep_update(body, row_data)
         meta_obj = core.Meta(**meta_data)
         payload = core.Payload(content_type='json', schema_shortname=schema_shortname,
                                body=f'{meta_obj.shortname}.json')
         meta_obj.payload = payload
         return meta_obj, body_data
+
+    def deep_update(self, body: dict, row_data):
+        for k, v in body.items():
+            if isinstance(v, dict):
+                self.deep_update(body.get(k, {}), row_data)
+            elif isinstance(v, str):
+                if v in row_data:
+                    body[k] = row_data[v]
+        return body
 
 
 creator = SpaceCreator()
