8d2d573afcf2af1177f991593a41e365503e0fde
amr <amr.emish@startappz.com>
2022-12-12 08:45:33 +0200

    update channels and support default values when null exists

--- src/loaders/load_pos_channels.py
+++ src/loaders/load_pos_channels.py
@@ -2,7 +2,7 @@ from utils.decorators import process_mapper
 from utils.default_loader import default_loader, meta_fixer
 
 
-@process_mapper(mapper="pos_channels", appended_list=["body.location.governorate_shortnames"])
+@process_mapper(mapper="pos_channels")
 def load(*args, **kwargs):
     default_loader(args, kwargs, apply_modifier)
     print("Successfully done.")
--- src/utils/mappers.py
+++ src/utils/mappers.py
@@ -3,6 +3,9 @@ import json
 import jsonschema
 from pathlib import Path
 
+from jsonschema import validators
+from jsonschema.validators import Draft4Validator
+
 
 class Mappers:
     mappers_path: str = 'mappers'
@@ -23,6 +26,25 @@ class Mappers:
         self.mappers = glob.glob(str(self.mappers_abs_path) + "/*.json")
         return len(self.mappers)
 
+    def extend_with_default(self, validator_class):
+        validate_properties = validator_class.VALIDATORS["properties"]
+
+        def set_defaults(validator, properties, instance, schema):
+            for property_, subschema in properties.items():
+                if "default" in subschema and not isinstance(instance, list):
+                    instance.setdefault(property_, subschema["default"])
+                    if instance.get(property_) is None:
+                        instance[property_] = subschema["default"]
+
+            for error in validate_properties(
+                    validator, properties, instance, schema,
+            ):
+                yield error
+
+        return validators.extend(
+            validator_class, {"properties": set_defaults},
+        )
+
     def validate_file_entry(self, json_filename, schema_shortname):
         schema_path = self.mappers_abs_path / "schema" / f'{schema_shortname}.json'
         json_path = self.mappers_abs_path / f'{json_filename}.json'
@@ -38,7 +60,8 @@ class Mappers:
         schema_path = self.mappers_abs_path / "schema" / f'{schema_shortname}.json'
         if not schema_path.is_file():
             raise Exception("Schema is not found in this path " + str(schema_path))
-        jsonschema.validate(instance=json_body, schema=json.loads(schema_path.read_bytes()))
+        validator = self.extend_with_default(Draft4Validator)
+        validator(json.loads(schema_path.read_bytes())).validate(json_body)
 
     def load(self, mapper_name):
         path = self.mappers_abs_path / f'{mapper_name}.json'
