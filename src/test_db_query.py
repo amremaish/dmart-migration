09a5e7f36929fe2f65ad9fccedc03526ab357f57
amr <amr.emish@startappz.com>
2022-12-20 13:54:04 +0200

    update test db query

--- src/test_db_query.py
+++ src/test_db_query.py
@@ -8,12 +8,13 @@ db_name = settings.db_name
 db_user = settings.db_user
 db_password = settings.db_password
 
+print(f'Connecting to {db_host} for database `{db_name}`')
 connection = cx_Oracle.connect(
     user=db_user,
     password=db_password,
     dsn=f'{db_host}:{db_port}/{db_name}')
 
-print(f'Connected to {db_host} for database `{db_name}`')
+print("Connected")
 query = "SELECT count(*) FROM SIM_SWAP LEFT JOIN POS_USER on POS_USER.ID = SIM_SWAP.POS_ID LEFT JOIN LOOKUP on LOOKUP.ID = SIM_SWAP.REASON WHERE REQUEST_TYPE = 'SIM-Swap'"
 print("-> Executing: " + query)
 cursor = connection.cursor()
