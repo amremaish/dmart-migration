93a32e60fcfee0184fe2d7ee5ec1ae3920b33258
amr <amr.emish@startappz.com>
2022-12-20 13:53:23 +0200

    update test db query

--- src/test_db_query.py
+++ src/test_db_query.py
@@ -1,20 +1,22 @@
 import cx_Oracle
 
-db_host = '10.50.9.106'
-db_port = '1526'
-db_name = 'mobtst'
-db_user = 'tele'
-db_password = 'tele123'
+from settings import settings
+
+db_host = settings.db_host
+db_port = settings.db_port
+db_name = settings.db_name
+db_user = settings.db_user
+db_password = settings.db_password
 
 connection = cx_Oracle.connect(
     user=db_user,
     password=db_password,
     dsn=f'{db_host}:{db_port}/{db_name}')
 
-query = "SELECT DISTRIBUTORS.NAME ,DISTRIBUTORS.MSISDN ,DISTRIBUTORS.STATUS ,DISTRIBUTORS.CREATION_DATE ,DISTRIBUTORS.UPDATED_DATE ,DISTRIBUTORS.PASSWORD ,DISTRIBUTORS.NAME_AR ,DISTRIBUTORS.NAME_KU ,DISTRIBUTORS.LANGUAGE_ID ,REGION.NAME_AR ,REGION.NAME_EN ,REGION.NAME_KU FROM DISTRIBUTORS INNER JOIN DISTRIBUTOR_REGION on DISTRIBUTORS.ID = DISTRIBUTOR_REGION.DISTRIBUTOR_ID INNER join REGION on REGION.ID = DISTRIBUTOR_REGION.REGION_ID OFFSET 0 ROWS FETCH NEXT 500 ROWS ONLY"
-print("-> Processing: " + query)
+print(f'Connected to {db_host} for database `{db_name}`')
+query = "SELECT count(*) FROM SIM_SWAP LEFT JOIN POS_USER on POS_USER.ID = SIM_SWAP.POS_ID LEFT JOIN LOOKUP on LOOKUP.ID = SIM_SWAP.REASON WHERE REQUEST_TYPE = 'SIM-Swap'"
+print("-> Executing: " + query)
 cursor = connection.cursor()
 cursor.execute(query)
 result = cursor.fetchall()
-print("")
 print(result)
