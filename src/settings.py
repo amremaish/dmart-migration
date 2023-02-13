99e02f0cb72aa4b03d78f1d176897b3cfc867a02
amr <amr.emish@startappz.com>
2023-02-13 05:23:01 +0200

    reset settings

--- src/settings.py
+++ src/settings.py
@@ -12,8 +12,8 @@ class Settings(BaseSettings):
     db_password: str = 'tele123'
     db_port: str = "1526"
     db_name: str = 'mobtst'
-    fetch_limit = 10
-    max_records = 1000
+    fetch_limit = 10000
+    max_records = -1
     db_driver: str = DBType.ORACLE
 
     class Config:
