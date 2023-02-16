87faa69b889b92e52f508742c32daa074ca482e3
amr <amr.emish@startappz.com>
2023-02-16 09:47:07 +0200

    add collaborators check info

--- src/loaders/load_check_info.py
+++ src/loaders/load_check_info.py
@@ -24,6 +24,12 @@ def apply_modifier(
 ):
     meta = meta_fixer(meta)
 
+    if meta.get('collaborators', {}).get('locked_by'):
+        meta['collaborators']['locked_by'] = creator.shortname_fixer(meta['collaborators']['locked_by'])
+    else:
+        if meta.get('collaborators', {}):
+            del meta['collaborators']
+
     meta['workflow_shortname'] = 'check_info'
     if meta.get('state'):
         if meta.get('state') == 'DELIVERED':
