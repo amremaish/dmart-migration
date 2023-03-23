21c15256bed56d732304b479523eb70f8c10bf90
amr <amr.emish@startappz.com>
2023-03-23 13:35:32 +0200

    update products schema

--- src/loaders/load_products.py
+++ src/loaders/load_products.py
@@ -46,7 +46,7 @@ def apply_modifier(
         role = None if len(role) == 0 else role[0]
         if role:
             if role == 'fs':
-                body['role_shortnames'] = ['franshise']
+                body['role_shortnames'] = ['franchise']
             elif role == 'supermarket':
                 body['role_shortnames'] = ['voucher_pos']
             elif role == 'ros':
