d9c3b87495793d5c807f337c161187ee875e80a0
amr <amr.emish@startappz.com>
2023-03-28 12:13:03 +0200

    update roles

--- src/loaders/load_backoffice.py
+++ src/loaders/load_backoffice.py
@@ -2,7 +2,7 @@ from utils.decorators import process_mapper
 from utils.default_loader import default_loader, meta_fixer, callback_fixer, msisdn_fixer
 
 
-@process_mapper(mapper="user_admins", remove_null_field=True)
+@process_mapper(mapper="backoffice", remove_null_field=True)
 def load(*args, **kwargs):
     default_loader(args, kwargs, apply_modifier)
     print("Successfully done.")
@@ -19,21 +19,7 @@ def apply_modifier(
         lookup: dict
 ):
     meta = meta_fixer(meta)
-    meta['shortname'] = f"admin_{meta['shortname']}"
-    dprt = body.get('department')
-    if not dprt:
-        body['department'] = "others"
-    else:
-        if dprt == 'B.O' or dprt == 'BO' or dprt == 'Back office Team' \
-                or dprt == 'Backoffice' or dprt == 'Back office'.strip():
-            body['department'] = "backoffice"
-        elif 'Sales' in dprt:
-            body['department'] = "sales"
-        elif dprt == 'Technical Support':
-            body['department'] = "technical_support"
-        else:
-            body['department'] = "others"
-
+    meta['shortname'] = f"bo_{meta['shortname']}"
     if meta.get('msisdn'):
         meta['msisdn'] = msisdn_fixer(meta.get('msisdn'))
 
