5bf4e610c50fb3f2fbd5133b2dc178079d02c6e2
amr <amr.emish@startappz.com>
2023-07-26 15:11:39 +0300

    add prints

--- src/loaders/load_pos_users.py
+++ src/loaders/load_pos_users.py
@@ -106,6 +106,8 @@ def apply_modifier(
     # set channel shortname
     if body.get('channel_shortname'):
         channel = channels.get(body['channel_shortname'])
+        print(body['channel_shortname'])
+        print(body['channel_shortname'] in channels)
         if channel:
             body['channel_shortname'] = channel
 
